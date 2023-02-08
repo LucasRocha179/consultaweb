189e1dbe861c978733d3cae8fe54fd6cf3e8da90
LucasRocha179 <96890752+LucasRocha179@users.noreply.github.com>
2023-02-07 21:56:26 -0300

    Update FormularioCadastro.jsx

--- pages/FormularioCadastro.jsx
+++ pages/FormularioCadastro.jsx
@@ -1,126 +1,61 @@
 import React, { Fragment, useState, useEffect } from "react";
-import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
+import { TextField, Button, Switch, FormControlLabel, makeStyles } from "@material-ui/core";
 import fireDb from '../firebase';
 
+const useStyles = makeStyles(theme => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+  },
+  textField: {
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    width: '100%',
+    marginTop: theme.spacing(2),
+  },
+  tableHead: {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.primary.contrastText,
+  },
+  tableCell: {
+    padding: theme.spacing(1),
+  },
+}));
+
 function FormularioCadastro() {
-    const [idUsuario, setIdUsuario] = useState("");
-    const [nome, setNome] = useState("");
-    const [sobrenome, setSobrenome] = useState("");
+  const classes = useStyles();
+  const [idUsuario, setIdUsuario] = useState("");
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
 
-    async function recuperarcdloja() {
-        fireDb.child('usuarios')
-            .orderByChild("cdloja").equalTo(nome).on('value', dbPhoto => {
-                //console.log(nome);
-                //console.log(dbPhoto.val());
-                
-                if (dbPhoto.val() != null) {
-                    const cdloja = JSON.stringify(dbPhoto.val());        
-                    const caracteres = cdloja.length;
-                    const resultado = cdloja.substring(cdloja.indexOf(":")+1);
-                    const resultado2 = resultado.length;
-                    const final = cdloja.substring(2,caracteres-resultado2-2);
-            
-                    atualizarTabela(final);
-                }
-                else{
-                    console.warn("Não foi possível encontrar sua OS");
-                }
-        })
-    }
+  async function recuperarcdloja() {
+    fireDb.child('usuarios')
+      .orderByChild("cdloja").equalTo(nome).on('value', dbPhoto => {
+        if (dbPhoto.val() != null) {
+          const cdloja = JSON.stringify(dbPhoto.val());
+          const caracteres = cdloja.length;
+          const resultado = cdloja.substring(cdloja.indexOf(":") + 1);
+          const resultado2 = resultado.length;
+          const final = cdloja.substring(2, caracteres - resultado2 - 2);
+          atualizarTabela(final);
+        } else {
+          console.warn("Não foi possível encontrar sua OS");
+        }
+      });
+  }
 
+  let [contactObjects, setContactObjects] = useState({});
 
-let [contactObjects, setContactObjects] = useState({});
-
-function atualizarTabela(recuperado) {
-    // fireDb.child('pacientes').orderByChild("email").equalTo(nome).on('value', dbPhoto => {
+  function atualizarTabela(recuperado) {
     const fire = fireDb.child('OS');
     fire.child(recuperado)
-        .child(sobrenome)
-        .orderByChild("numOS").equalTo(sobrenome).on('value', dbPhoto => {
-
-            if (dbPhoto.val() != null) {
-                setContactObjects({
-                    ...dbPhoto.val()
-                })
-            } else {
-                setContactObjects({})
-            }
-        })
-}
-
-
-return (
-
-    <Fragment>
-        <form
-            onSubmit={(event) => {
-                event.preventDefault();
-                recuperarcdloja();
-                
-            }}
-        >
-            <TextField
-                value={nome}
-                onChange={(event) => {
-                    let tmpNome = event.target.value;
-                    if (tmpNome.length >= 50) {
-                        tmpNome = tmpNome.substr(0, 50);
-                    }
-
-                    setNome(tmpNome);
-                }}
-                id="nome"
-                label="Código do Estabelecimento"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-            />
-            <TextField
-                value={sobrenome}
-                onChange={(event) => {
-                    setSobrenome(event.target.value);
-                }}
-                id="sobrenome"
-                label="Número da OS"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-            />
-
-            <Button type="submit" variant="contained" color="primary">
-                Consultar OS
-            </Button>
-        </form>
-
-        <div className="col-md-7">
-            <table className="table table-borderless table-stripped">
-                <thead className="thead-light">
-                    <tr>
-                        <th> Data             </th>
-                        <th> Status           </th>
-                        <th> OBS </th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {
-                        Object.keys(contactObjects).map(id => {
-                            return <tr key={id}>
-                                {/* <td> {contactObjects[id].fullName} </td>
-                                        <td> {contactObjects[id].celPhone} </td>
-                                        <td> {contactObjects[id].email} </td> */}
-                                <td> {contactObjects[id].data} </td>
-                                <td> {contactObjects[id].lancamento} </td>
-                                <td> {contactObjects[id].obs} </td>
-                            </tr>
-
-                        })
-                    }
-                </tbody>
-            </table>
-        </div>
-    </Fragment>
-);
-}
-
-export default FormularioCadastro;
+      .child(sobrenome)
+      .orderByChild("numOS").equalTo(sobrenome).on('value', dbPhoto => {
+        if (dbPhoto.val() != null) {
+          setContactObjects({
+            ...dbPhoto.val()
+          });
+        } else {
